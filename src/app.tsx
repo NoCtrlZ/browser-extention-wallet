63b3938d54a71bacd847ce99699f92767412e6ad
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-13 15:01:52 +0900

    set bamburger button

--- src/app.tsx
+++ src/app.tsx
@@ -23,7 +23,12 @@ export default class App extends React.Component<AppProps, AppState> {
                 <div id="header">
                     <div id="header-contents">
                         <div className="dropdown">
-                            <button type="button" id="hamburger-button" className="hamburger"></button>
+                        <div id="nav-drawer">
+                            <input id="nav-input" type="checkbox" className="nav-unshown"/>
+                            <label id="nav-open" htmlFor="nav-input"><span></span></label>
+                            <label className="nav-unshown" id="nav-close" htmlFor="nav-input"></label>
+                            <div id="nav-content"><h2>Select Function</h2></div>
+                        </div>
                         </div>
                         <div id="account-info">
                             <h2>Your Address</h2>
