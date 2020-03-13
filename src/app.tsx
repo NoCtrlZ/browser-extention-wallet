35f4c0e3075adddbaeb3a4d7014d770a51d8bfec
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-13 23:24:54 +0900

    divide component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import "./app.scss";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import "./app.scss"
+import Methods from "./methods"
 import Generate from "./generate"
 import Issue from "./issue"
 import Purchase from "./purchase"
@@ -30,28 +31,7 @@ export default class App extends React.Component<AppProps, AppState> {
                     </div>
                     <div id="header">
                         <div id="header-contents">
-                            <div className="dropdown">
-                            <div id="nav-drawer">
-                                <input id="nav-input" type="checkbox" className="nav-unshown"/>
-                                <label id="nav-open" htmlFor="nav-input"><span/></label>
-                                <label className="nav-unshown" id="nav-close" htmlFor="nav-input"/>
-                                <div id="nav-content">
-                                    <div className="sidebar-title">
-                                        <h2>Select Function</h2>
-                                    </div>
-                                    <div className="function-list">
-                                        <ul>
-                                            <li className="function-name"><Link to="/">Transfer</Link></li>
-                                            <li className="function-name"><Link to="/sign">Sign</Link></li>
-                                            <li className="function-name"><Link to="/generate">Generate</Link></li>
-                                            <li className="function-name"><Link to="/purchase">Purchase</Link></li>
-                                            <li className="function-name"><Link to="/issue">Issue</Link></li>
-                                            <li className="function-name"><Link to="/send">Send</Link></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
+                            <Methods/>
                             <div id="account-info">
                                 <h2>Your Address</h2>
                             </div>
---
+++ src/methods/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { HashRouter as Router, Link } from 'react-router-dom'
+
+const Methods = () =>
+<div id="nav-drawer">
+    <input id="nav-input" type="checkbox" className="nav-unshown"/>
+    <label id="nav-open" htmlFor="nav-input"><span/></label>
+    <label className="nav-unshown" id="nav-close" htmlFor="nav-input"/>
+    <div id="nav-content">
+        <div className="sidebar-title">
+            <h2>Select Function</h2>
+        </div>
+        <div className="function-list">
+            <ul>
+                <li className="function-name"><Link to="/">Transfer</Link></li>
+                <li className="function-name"><Link to="/sign">Sign</Link></li>
+                <li className="function-name"><Link to="/generate">Generate</Link></li>
+                <li className="function-name"><Link to="/purchase">Purchase</Link></li>
+                <li className="function-name"><Link to="/issue">Issue</Link></li>
+                <li className="function-name"><Link to="/send">Send</Link></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+export default Methods
