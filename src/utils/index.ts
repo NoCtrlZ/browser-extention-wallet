fda348e020bfb104127ef26768eb138826654b29
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-25 23:31:43 +0900

    public key generate

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,10 @@
 import Wallet from './key'
+import {hash} from './key'
 
 const walletInit = () => {
     let wallet = new Wallet()
-    console.log(wallet.privateKey.pairs[0])
+    console.log(wallet.publicKey.pairs[0])
+    console.log(hash('hllo'))
 }
 
 walletInit()
--- src/utils/key.ts
+++ src/utils/key.ts
@@ -1,3 +1,5 @@
+import { SHA256 } from 'crypto-ts'f
+
 const PRIVATE_KEY_LENGT = 256
 const SIGNATURE_LENGT = 256
 const U256_MAX_VALUE = 2 ** 256
@@ -6,31 +8,34 @@ export default class Wallet {
     privateKey: PrivateKey
     publicKey: PublicKey
     constructor() {
-        this.privateKey = new PrivateKey()
-        this.publicKey = new PublicKey()
+        let prvPairs = [],
+        pubPairs = []
+        for(let i = 0; i < PRIVATE_KEY_LENGT; i++) {
+            let [adam, eve] = prvKeyPair();
+            let prvPair: [bigint, bigint] = [adam, eve]
+            let pubPair: [bigint, bigint] = [hash(String(adam)), hash(String(eve))]
+            prvPairs.push(prvPair)
+            pubPairs.push(pubPair)
+        }
+        this.privateKey = new PrivateKey(prvPairs)
+        this.publicKey = new PublicKey(pubPairs)
     }
 }
 
 class PrivateKey {
     pairs: Array<[bigint, bigint]>
-    constructor() {
-        let pairs = []
-        for(let i = 0; i < PRIVATE_KEY_LENGT; i++) {
-            let pair: [bigint, bigint] = [random_uint256(), random_uint256()]
-            pairs.push(pair)
-        }
+    constructor(pairs: Array<[bigint, bigint]>) {
         this.pairs = pairs;
     }
 }
 
 class PublicKey {
     pairs: Array<[bigint, bigint]>
-    constructor() {
-        let pairs = []
-        let pair: [bigint, bigint] = [BigInt(0), BigInt(0)]
-        pairs.push(pair)
+    constructor(pairs: Array<[bigint, bigint]>) {
         this.pairs = pairs;
     }
 }
 
-const random_uint256 = () => BigInt(Math.floor(Math.random() * U256_MAX_VALUE))
+const prvKeyPair = () => [randomUint256(), randomUint256()]
+const randomUint256 = () => BigInt(Math.floor(Math.random() * U256_MAX_VALUE))
+export const hash = (uint256: string) => SHA256(uint256)
